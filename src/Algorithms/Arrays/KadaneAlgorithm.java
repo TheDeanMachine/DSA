d32ed8bc8e602918504d9a0dcc613b1042d013ce
Vadym Feskin <Vadym.feskin@gmail.com>
2022-06-06 11:44:04 -0700

    Added previously completed LeetCode Solutions

--- src/Algorithms/Arrays/KadaneAlgorithm.java
+++ src/Algorithms/Arrays/KadaneAlgorithm.java
@@ -2,34 +2,20 @@ package Algorithms.Arrays;
 
 public class KadaneAlgorithm {
 
-    // DP solution
-    public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int[] dp = new int[nums.length + 1]; //dp(i) = max sum of subarray ending at (i-1)'th index
-        int i = 1;
-
-        for(int num: nums) {
-            dp[i] = Math.max(dp[i-1] + num, num);
-            max = Math.max(max, dp[i++]);
-        }
-
-        return max;
-    }
-
-    // Kadane's Algorithm (Optimization of Above DP solution):
-    // Since we only need DP[i-1] to calculate DP[i].
-    // There is no need of storing all values, we can get our solution done by one variable as well.
-    public int maxSubArray2(int[] nums) {
+    public static int maxSubArray(int[] nums) {
         int curr = 0;
         int max = Integer.MIN_VALUE;
 
         for(int num: nums) {
-            // Same as DP[i] = Math.max(DP[i-1] + num, num);
             curr = Math.max(curr + num, num);
             max = Math.max(max, curr);
         }
         return max;
     }
 
+    public static void main(String[] args) {
+        int[]  array = {1, 3, 8, -2, 6, -8, 5};
+        System.out.println(maxSubArray(array));
+    }
 
 }
