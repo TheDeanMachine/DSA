9c4b812d61f32d99fd64667f27f6a08bacb78184
Vadym Feskin <Vadym.feskin@gmail.com>
2022-06-09 11:42:13 -0700

    Added stack LeetCode Solutions

--- src/DataStructures/StacksAndQueues/Stacks/Solution.java
+++ src/DataStructures/StacksAndQueues/Stacks/Solution.java
@@ -10,21 +10,27 @@ public class Solution {
      *  1. Open brackets must be closed by the same type of brackets.
      *  2. Open brackets must be closed in the correct order.
      */
-    public boolean isValid(String s) {
+    public static boolean isValid(String s) {
         if (s.length() % 2 != 0) return false;
 
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {         // check for matching closing brace
                 stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {  // check for matching closing brace
                 stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {  // check for matching closing brace
                 stack.pop();
             } else {
-                stack.push(c);
+                stack.push(c); // add opening braces to the stack
             }
         }
         return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("{{({})}}"));
     }
 }
