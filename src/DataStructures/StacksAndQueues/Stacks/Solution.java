fd5d0f2846e97cb4f06db3b5b4679e331764f6c7
Vadym Feskin <Vadym.feskin@gmail.com>
2022-06-13 10:05:14 -0700

    Next Greatest element in array

--- src/DataStructures/StacksAndQueues/Stacks/Solution.java
+++ src/DataStructures/StacksAndQueues/Stacks/Solution.java
@@ -28,8 +28,49 @@ public class Solution {
         return stack.isEmpty();
     }
 
+    /**
+     * Print out the next greater element for every element in the array.
+     * The next greater element for a given element is the first greater element on the right side of the array.
+     * If no greater element exists, then the next greater element is a sentinel value, -1
+     */
+    public static void printNextGreaterElement(int[] arr) {
+        if (arr.length == 0) {
+            System.out.println();
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            int next = arr[i];
+            if (!stack.isEmpty()) {
+                int popped = stack.pop();
+                while (popped < next) {
+                    System.out.println(popped + " --> " + next);
+                    if (stack.isEmpty()) {
+                        break;
+                    }
+                    popped = stack.pop();
+                }
+
+                if (popped > next) {
+                    stack.push(popped);
+                }
+            }
+            stack.push(next);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + " --> " + -1);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(isValid("()[]{}"));
         System.out.println(isValid("{{({})}}"));
+
+        printNextGreaterElement(new int[]{16, 7, 2, 15});
+
     }
 }
